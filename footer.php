0594b2a9a3232048d0fba0a83c867e53ecd7b7d4
roguckas <almantas06@gmail.com>
2019-12-27 14:37:40 +0200

    Titulinis: "Susisiekime" section.

--- footer.php
+++ footer.php
@@ -4,9 +4,7 @@ $footer = "<footer class=\"footer\">
         <button class=\"btn col-sm-12 col-md-8\">Pradėti naudotis</button>
         <div class=\"footer-info\">
             <h3>Reikalinga papildoma informacija?<br> Susisiekime!</h3>
-            <a href=\"tel:+37000000000\">+370 000 000 00</a>
             <br>
-            <a href=\"mailto:info@orouostai.lt\">info@orouostai.lt</a>
         </div>
         <button class=\"btn col-sm-12 col-md-2 mt-5\">
         <a class='text-white' href='duk.php'>D.U.K</a>
