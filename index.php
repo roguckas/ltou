aae034b305d320f051c7a74a4f05f87bcc9f2155
roguckas <almantas06@gmail.com>
2020-01-30 11:26:37 +0200

    Logos update.

--- index.php
+++ index.php
@@ -193,26 +193,31 @@
                     <h2>Prisijunk prie tų, kurie jau naudojasi</h2>
                     <br>
                     <div class="row justify-content-center col-12">
-                        <img class="col-6 col-md-3  h-50 mr-md-5" src="images/logos/tez-tour.png">
-                        <img class="col-6 col-md-3 h-50 mr-md-5" src="images/logos/investuok-lietuvoje.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/govilnius.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/paysera.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/samsung.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/citybee.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/dpc-02.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/tui-logo.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/makalius.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/huawei.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/grohe.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/gameinsight.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/narbutas.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/airbaltic.60568592.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/ignitis.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/optibet.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/Logo_Hegelmann_LT.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/park-inn.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/mega.svg">
-                        <img class="col-6 col-md-3 mr-md-5 mt-3" src="images/logos/akropolis.svg">
+                        <img class="col-6 col-md-3 mr-md-5" src="images/logos/tez-tour.png">
+                        <img class="col-6 col-md-3 mr-md-5" src="images/logos/investuok-lietuvoje.svg">
+                        <img class="col-6 col-md-3 mr-md-5" src="images/logos/govilnius.svg">
+
+                        <img class="col-6 col-md-2 mr-md-5 mt-5" src="images/logos/paysera.svg">
+                        <img class="col-6 col-md-2 mr-md-5 mt-5" src="images/logos/optibet.svg">
+                        <img class="col-6 col-md-2 mr-md-5 mt-5" src="images/logos/narbutas.svg">
+                        <img class="col-6 col-md-2 mr-md-5 mt-5" src="images/logos/dpc-02.svg">
+
+                        <img class="col-6 col-md-3 mr-md-5" src="images/logos/Huawei.svg">
+                        <img class="col-6 col-md-3 mr-md-5" src="images/logos/Logo_Hegelmann_LT.svg">
+                        <img class="col-6 col-md-2 mr-md-5" src="images/logos/samsung.svg">
+
+                        <img class="col-6 col-md-2 mr-md-5" src="images/logos/citybee.svg">
+                        <img class="col-6 col-md-2 mr-md-5" src="images/logos/gameinsight.svg">
+                        <img class="col-6 col-md-2 mr-md-5" src="images/logos/airbaltic.60568592.svg">
+                        <img class="col-6 col-md-2 mr-md-5" src="images/logos/grohe.svg">
+
+                        <img class="col-6 col-md-2 mr-md-5 mt-4" src="images/logos/akropolis.svg">
+                        <img class="col-6 col-md-2 mr-md-5 mt-4" src="images/logos/tui-logo.svg">
+                        <img class="col-6 col-md-2 mr-md-5 mt-5" src="images/logos/ignitis.svg">
+                        <img class="col-6 col-md-2 mr-md-5 mt-4" src="images/logos/mega.svg">
+
+                        <img class="col-6 col-md-2 mr-md-5 mt-5" src="images/logos/makalius.svg">
+                        <img class="col-6 col-md-2 mr-md-5 mt-5" src="images/logos/park-inn.svg">
                     </div>
                 </div>
             </div>
