21bf28683434f4ff9dabb61537e3ddeb296aa70f
almantas <almantas06@gmail.com>
2022-05-22 12:08:11 +0300

    Contact update.

--- contact.php
+++ contact.php
@@ -16,43 +16,12 @@ include 'header.php';
     <div class="row justify-content-center">
         <div class="col col-md-6 col-xl-4 mb-5 pb-5  row">
             <div class="col-12 row justify-content-center bg-white text-center mt-5 mt-lg-0 mr-lg-5 shadow rounded">
-<!--                <div class="mt-lg-n5">-->
-<!--                    <img src="images/young-man-1281282_1920.png">-->
-<!--                </div>-->
                 <div class="col-12 justify-content-md-center py-5">
-                    <h3 class="col-12 mb-2 text-primary">Eglė Lydekaitytė</h3>
-                    <p class="col-12 text-dark mb-5">Pardavimų vadovė</p>
-                    <a class="col-12 text-dark my-5" href="tel:+37068286446">+37068286446</a><br>
+                    <h3 class="col-12 mb-2 text-primary">Gerda Ivaškevičienė</h3>
+                    <p class="col-12 text-dark mb-5">Pardavimo vadovė</p>
+                    <a class="col-12 text-dark my-5" href="tel:+37061033293">+370 610 33293</a><br>
                     <a class="col-12 text-dark my-5" href="mailto:e.lydekaityte@ltou.lt">e.lydekaityte@ltou.lt<br></a>
                     <a class="col-12 my-2 website text-left" href="https://www.vno.lt/" target="_blank">www.vno.lt</a>
-                </div>
-            </div>
-        </div>
-        <div class="col col-md-6 col-xl-4 mb-5 pb-5 row">
-            <div class="col-12 row justify-content-center bg-white text-center mt-5 mt-lg-0 mr-lg-5 shadow rounded">
-<!--                <div class="mt-lg-n5">-->
-<!--                    <img src="images/young-man-1281282_1920.png">-->
-<!--                </div>-->
-                <div class="col-12 justify-content-md-center py-5">
-                    <h3 class="col-12 mb-2 text-primary">MONIKA SIMONAITĖ</h3>
-                    <p class="col-12 text-dark mb-5">Pardavimų vadovė Kaune</p>
-                    <a class="col-12 text-dark my-5" href="tel:+370 614 36286">+370 614 36286</a><br>
-                    <a class="col-12 text-dark my-5" href="mailto:m.simonaite@ltou.lt">m.simonaite@ltou.lt<br></a>
-                    <a class="col-12 my-2 website text-left" href="https://www.kaunas-airport.lt/" target="_blank">www.kun.lt</a>
-                </div>
-            </div>
-        </div>
-        <div class="col col-md-6 col-xl-4 mb-5 pb-5 row">
-            <div class="col-12 row justify-content-center bg-white text-center mt-5 mt-lg-0 mr-lg-5 shadow">
-<!--                <div class="mt-lg-n5 rounded-circle">-->
-<!--                    <img src="images/young-man-1281282_1920.png">-->
-<!--                </div>-->
-                <div class="col-12 justify-content-md-center py-5">
-                    <h3 class="col-12 mb-2 text-primary">Marius Naraveckas</h3>
-                    <p class="col-12 text-dark mb-5">Pardavimų vadovas</p>
-                    <a class="col-12 text-dark my-5" href="tel:+37061522086">+370 615 22 086</a><br>
-                    <a class="col-12 text-dark my-5" href="mailto:m.naraveckas@ltou.lt">m.naraveckas@ltou.lt<br></a>
-                    <a class="col-12 my-2 website text-left" href="https://www.palanga-airport.lt/lt/" target="_blank">www.palanga-airport.lt</a>
                 </div>
             </div>
         </div>
--- footer.php
+++ footer.php
@@ -7,45 +7,14 @@ $footer = "<footer class=\"footer\">
 
         <section class=\"container\" id=\"container-fluid services\">
     <div class=\"row justify-content-center\">
-        <div class=\"col col-md-6 col-xl-4 mb-5 pb-md-5  row\">
-            <div class=\"col-12 row justify-content-center bg-white text-center mt-5 mt-lg-0 mr-lg-5 shadow contact-person\">
-<!--                <div class=\"mt-lg-n5\">-->
-<!--                    <img src=\"images/young-man-1281282_1920.png\">-->
-<!--                </div>-->
+        <div class=\"col col-md-6 col-xl-4 mb-5 pb-5  row\">
+            <div class=\"col-12 row justify-content-center bg-white text-center mt-5 mt-lg-0 mr-lg-5 shadow rounded\">
                 <div class=\"col-12 justify-content-md-center py-5\">
-                    <h3 class=\"col-12 mb-2 text-primary\">GINTARĖ NORVILAITĖ-TAUTEVIČĖ</h3>
-                    <p class=\"col-12 text-dark mb-5\">Pardavimų skyriaus vadovė Vilniuje</p>
-                    <a class=\"col-12 text-dark my-5\" href=\"tel:+370 61680939\">+370 616 80939</a><br>
-                    <a class=\"col-12 text-dark my-5\" href=\"mailto:g.norvilaite-tautevice@ltou.lt\">g.norvilaite-tautevice@ltou.lt<br></a>
-                    <a class=\"col-12 my-2 website text-left\" href=\"https://www.vno.lt/\" target=\"_blank\">www.vno.lt</a>
-                </div>
-            </div>
-        </div>
-        <div class=\"col col-md-6 col-xl-4 mb-5 pb-md-5 row\">
-            <div class=\"col-12 row justify-content-center bg-white text-center mt-5 mt-lg-0 mr-lg-5 shadow contact-person\">
-<!--                <div class=\"mt-lg-n5\">-->
-<!--                    <img src=\"images/young-man-1281282_1920.png\">-->
-<!--                </div>-->
-                <div class=\"col-12 justify-content-md-center py-5\">
-                    <h3 class=\"col-12 mb-2 text-primary\">MONIKA SIMONAITĖ</h3>
-                    <p class=\"col-12 text-dark mb-5\">Pardavimo vadovė Kaune</p>
-                    <a class=\"col-12 text-dark my-5\" href=\"tel:+37061436286\">+370 614 36286</a><br>
-                    <a class=\"col-12 text-dark my-5\" href=\"mailto:m.simonaite@ltou.lt\">m.simonaite@ltou.lt<br></a>
-                    <a class=\"col-12 my-2 website text-left\" href=\"https://www.kaunas-airport.lt/\" target=\"_blank\">www.kun.lt</a>
-                </div>
-            </div>
-        </div>
-        <div class=\"col col-md-6 col-xl-4 mb-5 pb-md-5 row\">
-            <div class=\"col-12 row justify-content-center bg-white text-center mt-5 mt-lg-0 mr-lg-5 shadow contact-person\">
-<!--                <div class=\"mt-lg-n5 rounded-circle\">-->
-<!--                    <img src=\"images/young-man-1281282_1920.png\">-->
-<!--                </div>-->
-                <div class=\"col-12 justify-content-md-center py-5\">
-                    <h3 class=\"col-12 mb-2 text-primary\">GINTARĖ NORVILAITĖ-TAUTEVIČĖ</h3>
-                    <p class=\"col-12 text-dark mb-5\">Pardavimų skyriaus vadovė Palangoje</p>
-                    <a class=\"col-12 text-dark my-5\" href=\"tel:+37061680939\">+370 616 80939</a><br>
-                    <a class=\"col-12 text-dark my-5\" href=\"mailto:g.norvilaite-tautevice@ltou.lt\">g.norvilaite-tautevice@ltou.lt<br></a>
-                    <a class=\"col-12 my-2 website text-left\" href=\"https://www.palanga-airport.lt/lt/\" target=\"_blank\">www.palanga-airport.lt</a>
+                    <h3 class=\"col-12 mb-2 text-primary\">Gerda Ivaškevičienė</h3>
+                    <p class=\"col-12 text-dark mb-5\">Pardavimo vadovė</p>
+                    <a class=\"col-12 text-dark my-5\" href=\"tel:+37061033293\">+370 610 33293</a><br>
+                    <a class=\"col-12 text-dark my-5\" href=\"mailto:G.Ivaskeviciene@ltou.lt\">G.Ivaskeviciene@ltou.lt<br></a>
+                    <a class=\"col-12 my-2 website text-left\" href=\"https://www.ltou.lt/\" target=\"_blank\">www.ltou.lt</a>
                 </div>
             </div>
         </div>
